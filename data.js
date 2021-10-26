3e331f82bd8cae40af9796f730cad0a5e5ae9c53
9919004330 <87610308+9919004330@users.noreply.github.com>
2021-10-26 13:22:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+const data={
+    C371:'https://meet.google.com/lookup/cyxnk27dy5?authuser=2&hs=179',
+    C272:'https://meet.google.com/lookup/cvljaaeseh',
+    C452:'https://meet.google.com/oyq-nupk-zvp',
+    C449:'http://meet.google.com/jxa-vsxc-int',
+    C453:'#'
+
+}
